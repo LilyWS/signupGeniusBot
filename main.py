01cd5bfed8edefe5603b2727a99f94f27eec44a8
LilyWS <stranger1212ss23@gmail.com>
2022-10-03 01:25:50 -0400

    finished

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from operator import sub
 from time import sleep
 import sys
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Firefox()
-driver.get("https://www.signupgenius.com/go/8050848afa62ba1fb6-wohho")
+#python main.py https://www.signupgenius.com/go/8050848afa62ba1fb6-wohho "12:00pm - 2:00pm"
 
-specifiedRow = driver.find_element(By.XPATH, '//*[contains (text (),"12:00pm - 2:00pm")]/ancestor::tr[1]')
+driver = webdriver.Firefox()
+driver.get(sys.argv[1])
+
+specifiedRow = driver.find_element(By.XPATH, f'//*[contains (text (),"{sys.argv[2]}")]/ancestor::tr[1]')
 signupCheckBox = specifiedRow.find_element(By.NAME, "siid")
 driver.execute_script("arguments[0].scrollIntoView();", signupCheckBox)
 signupCheckBox.click()
@@ -17,16 +21,24 @@ print(specifiedRow.text)
 print(submitButton.text)
 submitButton.click()
 
-sleep(5) #wait for page load, will use selenium wait function later
+timeout = 30
+
+try:
+    element_present = EC.presence_of_element_located((By.NAME, 'btnSignUp'))
+    WebDriverWait(driver, timeout).until(element_present)
+except TimeoutException:
+    print("Timed out :(")
+
+sleep(1)
 
 firstNameInput = driver.find_element(By.ID, 'firstname')
 lastNameInput = driver.find_element(By.ID, 'lastname')
 emailInput = driver.find_element(By.ID, 'email')
 signUpBtn = driver.find_element(By.NAME, 'btnSignUp')
 print(firstNameInput)
-firstNameInput.send_keys('Lily')
-lastNameInput.send_keys('Sperber')
-emailInput.send_keys('stranger1212ss23@gmail.com')
+firstNameInput.send_keys(sys.argv[3])
+lastNameInput.send_keys(sys.argv[4])
+emailInput.send_keys(sys.argv[5])
 signUpBtn.click()
 sleep(5)
 driver.close()
